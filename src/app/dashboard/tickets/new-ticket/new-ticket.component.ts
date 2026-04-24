f8979b927fb5d76be1f92f208cb2f635ad2b8e75
Veronika Kuzmina <veronika@Veronikas-MacBook-Pro.local>
2026-04-24 17:01:20 +0200

    Logic updated, styles improved

---
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../shared/button/button.component';
+import { FormControlComponent } from '../../../shared/form-control/form-control.component';
+
+@Component({
+  selector: 'app-new-ticket',
+  standalone: true,
+  imports: [ButtonComponent, FormControlComponent],
+  templateUrl: './new-ticket.component.html',
+  styleUrl: './new-ticket.component.css',
+})
+export class NewTicketComponent {}
